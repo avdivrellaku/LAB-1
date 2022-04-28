20a2133fe4a3581651c47951fec9c2c83faebd5f
Luaniiii <93317143+Luaniiii@users.noreply.github.com>
2022-04-28 23:30:39 +0200

    team controller update

--- LAB 1/Controllers/TeamController.cs
+++ LAB 1/Controllers/TeamController.cs
@@ -22,12 +22,62 @@ namespace LAB_1.Controllers
 
 
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Models.Team>>> Get(String id)
+        {
+            var team = await this.context.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return BadRequest("Player not found.");
+            }
+            return Ok(team);
+
+        }
+
         [HttpPost]
         public async Task<ActionResult<List<Models.Team>>> AddTeams(Models.Team team)
         {
             this.context.Teams.Add(team);
             await this.context.SaveChangesAsync();
             return Ok(await this.context.Teams.ToListAsync());
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Models.Team>>> UpdateTeams(Models.Team team)
+        {
+
+            var dbTeam = await this.context.Teams.FindAsync(team.Id);
+            if (dbTeam == null)
+            {
+                return BadRequest("Team not found.");
+            }
+            dbTeam.Name = team.Name;
+            dbTeam.Owner = team.Owner;
+            dbTeam.Coach = team.Coach;
+            dbTeam.Division = team.Division;
+            dbTeam.Conference = team.Conference;
+
+            await this.context.SaveChangesAsync();
+
+            return Ok(await this.context.SaveChangesAsync());
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Models.Team>>> DeleteTeam(String id)
+        {
+            var team = await this.context.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return BadRequest("Player not found.");
+            }
+
+
+            this.context.Teams.Remove(team);
+            await this.context.SaveChangesAsync();
+            return Ok(await this.context.SaveChangesAsync());
 
         }
     }
